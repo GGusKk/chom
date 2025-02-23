803ad954f0aa97ec25f41ad14489e5fe306e7733
GGusKk <nongtarakorn@gmail.com>
2025-02-23 22:19:25 +0700

    Add files via upload

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -4,64 +4,83 @@ export default function Card() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-4 mb-4">
-          <div className="card">
-            <a href="https://line.me/R/ti/p/@278djxdt?ts=02222002&oat_content=url" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/img/1115.png"
-                layout="responsive"
-                width={500}
-                height={300}
-                alt="Phantasy Star Online 2 Landing Page"
-                className="card-img-top"
-              />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">LINE OFFICIAL</h5>
-              <p className="card-text">
-                
-              </p>
-              <a href="https://line.me/R/ti/p/@278djxdt?ts=02222002&oat_content=url" className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-                Click
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 mb-4">
-          <div className="card">
-            <a href="https://www.facebook.com/profile.php?id=61573710261725" target="_blank" rel="noopener noreferrer">
-              <Image
-                src="/assets/img/1116.png"
-                layout="responsive"
-                width={500}
-                height={300}
-                alt="Phantasy Star Online 2 New Genesis Steam"
-                className="card-img-top"
-              />
-            </a>
-            <div className="card-body">
-              <h5 className="card-title">FACEBOOK OFFICIAL</h5>
-              <p className="card-text">
-               
-              </p>
-              <a href="https://www.facebook.com/profile.php?id=61573710261725" className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-                Click
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 mb-4" >
+      <div className="col-md-4 mb-4">
   <div className="card">
-    <a href="https://store.epicgames.com/th/p/phantasy-star-online-2" target="_blank" rel="noopener noreferrer">
-      <img
-        src="https://media1.giphy.com/media/kZqbBT64ECtjy/giphy.gif?cid=6c09b952ua1655blt74fqn8r9c4dj23nuq9p3jmm19b80t3k&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g"
-        alt="Hello Kitty Pink GIF"
+    <a href="https://www.facebook.com/profile.php?id=61573710261725" target="_blank" rel="noopener noreferrer">
+      <Image
+        src="/assets/img/1115.png"
+        layout="responsive"
+        width={500}
+        height={300}
+        alt="Phantasy Star Online 2 New Genesis Steam"
         className="card-img-top"
-        style={{ width: '100%', height: 'auto' }}
       />
     </a>
+    <div className="card-body">
+      <h5 className="card-title">Smart Navigator Wheelchair V.1</h5>
+      <p className="card-text">
+        <strong>ฟังก์ชัน:</strong> รถเข็นอัจฉริยะล้ำสมัย มาพร้อมระบบ GPS, ควบคุมผ่านมือถือ, ล้อไฟฟ้าอัจฉริยะ และดีไซน์ล้ำยุค<br />
+        <strong>ลักษณะ:</strong> ดีไซน์โมเดิร์น ไฟ LED ส่องสว่าง เพิ่มความปลอดภัยและความสะดวกในการใช้งาน<br />
+        <strong></strong>  “ที่สุดของนวัตกรรมรถเข็นอัจฉริยะ สั่งงานผ่านมือถือ ควบคุมง่าย พร้อมดีไซน์ล้ำอนาคต!”
+      </p>
+      <a href="https://www.facebook.com/profile.php?id=61573710261725" className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+        สั่งซื้อ
+      </a>
+    </div>
+  </div>
+</div>
+
+
+        <div className="col-md-4 mb-4">
+  <div className="card">
+    <a href="https://www.facebook.com/profile.php?id=61573710261725" target="_blank" rel="noopener noreferrer">
+      <Image
+        src="/assets/img/1116.png"
+        layout="responsive"
+        width={500}
+        height={300}
+        alt="Phantasy Star Online 2 New Genesis Steam"
+        className="card-img-top"
+      />
+    </a>
+    <div className="card-body">
+      <h5 className="card-title">Smart Navigator Wheelchair V.2 </h5>
+      <p className="card-text">
+        <strong>ฟังก์ชัน:</strong> รถเข็นไฟฟ้าอัจฉริยะ รองรับระบบ GPS, ตรวจจับสิ่งกีดขวาง, ปรับความเร็วอัตโนมัติ เหมาะสำหรับสนามบินและพื้นที่สาธารณะ<br />
+        <strong>ลักษณะ:</strong> รูปทรงเรียบหรู ดูทันสมัย พร้อมเทคโนโลยีช่วยการเดินทาง<br />
+        <strong></strong>  “อิสระทุกการเดินทาง ด้วยรถเข็นอัจฉริยะ GPS นำทาง ตรวจจับสิ่งกีดขวาง ใช้งานง่ายในทุกพื้นที่!”
+      </p>
+      <a href="https://www.facebook.com/profile.php?id=61573710261725" className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+        สั่งซื้อ
+      </a>
+    </div>
+  </div>
+</div>
+
+
+<div className="col-md-4 mb-4">
+  <div className="card">
+    <a href="https://www.facebook.com/profile.php?id=61573710261725" target="_blank" rel="noopener noreferrer">
+      <Image
+        src="/assets/img/1120.png"
+        layout="responsive"
+        width={500}
+        height={300}
+        alt="Smart Electric Wheelchair"
+        className="card-img-top"
+      />
+    </a>
+    <div className="card-body">
+      <h5 className="card-title">FACEBOOK</h5>
+      <p className="card-text">
+        <strong>ฟังก์ชัน:</strong> รถเข็นไฟฟ้าสำหรับการใช้งานในเมือง รองรับการขับเคลื่อนด้วยพลังงานไฟฟ้า โครงสร้างแข็งแรง คล่องตัวสูง<br />
+        <strong>ลักษณะ:</strong> ดีไซน์เรียบหรู ทันสมัย เหมาะกับการใช้งานในชีวิตประจำวัน<br />
+        <strong></strong> “คล่องตัวทุกที่ สะดวกทุกการเดินทาง รถเข็นไฟฟ้าดีไซน์เรียบหรู ใช้งานง่ายในเมือง”
+      </p>
+      <a href="https://www.facebook.com/profile.php?id=61573710261725" className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+        สั่งซื้อ
+      </a>
+    </div>
   </div>
 </div>
 
